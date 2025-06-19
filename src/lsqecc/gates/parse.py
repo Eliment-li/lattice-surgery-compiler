0a6ed42cf91f8dd72d29ba69779335fc3f0a17fc
Eliment-li <felix.litian@foxmail.com>
2025-06-19 22:39:11 +0800

    feat: handle cx gate in qasm file

--- src/lsqecc/gates/parse.py
+++ src/lsqecc/gates/parse.py
@@ -71,6 +71,12 @@ def parse_gates_circuit(qasm: str) -> Sequence[gates.Gate]:
                     phase=Fraction(1, phase_pi_frac_den),
                 )
             )
+        elif instruction.startswith('cx'):
+            if len(args) != 2:
+                raise QasmParseException(f"CNOT instruction requires exactly 2 args, got {len(args)}")
+            ret_gates.append(
+                gates.CNOT(control_qubit=get_index_arg(args[0]), target_qubit=get_index_arg(args[1]))
+            )
         elif not instruction and not args:
             pass
         else:
