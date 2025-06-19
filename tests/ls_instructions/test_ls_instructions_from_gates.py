a34f8969baf1e4ac250b8c4e7d4558703c78ff20
Eliment-li <felix.litian@foxmail.com>
2025-06-19 22:39:28 +0800

    add test code

--- tests/ls_instructions/test_ls_instructions_from_gates.py
+++ tests/ls_instructions/test_ls_instructions_from_gates.py
@@ -1,4 +1,5 @@
 import pytest
+from qiskit import QuantumCircuit
 
 from lsqecc.gates.gates_circuit import GatesCircuit
 from lsqecc.ls_instructions.ls_instructions_from_gates import (
@@ -28,6 +29,7 @@ TEST2 = """OPENQASM 2.0;
 include "qelib1.inc";
 qreg q[3];
 h q[0];
+cx q[0],q[1];
 crz(pi/2) q[1],q[0];
 crz(pi/4) q[2],q[0];
 h q[1];
@@ -38,61 +40,68 @@ h q[2];
 TEST = """OPENQASM 2.0;
 include "qelib1.inc";
 qreg q[10];
+creg c[10];
+creg meas[10];
 h q[9];
-crz(pi/2) q[9],q[8];
+cp(pi/2) q[9],q[8];
 h q[8];
-crz(pi/4) q[9],q[7];
-crz(pi/2) q[8],q[7];
+cp(pi/4) q[9],q[7];
+cp(pi/2) q[8],q[7];
 h q[7];
-crz(pi/8) q[9],q[6];
-crz(pi/4) q[8],q[6];
-crz(pi/2) q[7],q[6];
+cp(pi/8) q[9],q[6];
+cp(pi/4) q[8],q[6];
+cp(pi/2) q[7],q[6];
 h q[6];
-crz(pi/16) q[9],q[5];
-crz(pi/8) q[8],q[5];
-crz(pi/4) q[7],q[5];
-crz(pi/2) q[6],q[5];
+cp(pi/16) q[9],q[5];
+cp(pi/8) q[8],q[5];
+cp(pi/4) q[7],q[5];
+cp(pi/2) q[6],q[5];
 h q[5];
-crz(pi/32) q[9],q[4];
-crz(pi/16) q[8],q[4];
-crz(pi/8) q[7],q[4];
-crz(pi/4) q[6],q[4];
-crz(pi/2) q[5],q[4];
+cp(pi/32) q[9],q[4];
+cp(pi/16) q[8],q[4];
+cp(pi/8) q[7],q[4];
+cp(pi/4) q[6],q[4];
+cp(pi/2) q[5],q[4];
 h q[4];
-crz(pi/64) q[9],q[3];
-crz(pi/32) q[8],q[3];
-crz(pi/16) q[7],q[3];
-crz(pi/8) q[6],q[3];
-crz(pi/4) q[5],q[3];
-crz(pi/2) q[4],q[3];
+cp(pi/64) q[9],q[3];
+cp(pi/32) q[8],q[3];
+cp(pi/16) q[7],q[3];
+cp(pi/8) q[6],q[3];
+cp(pi/4) q[5],q[3];
+cp(pi/2) q[4],q[3];
 h q[3];
-crz(pi/128) q[9],q[2];
-crz(pi/64) q[8],q[2];
-crz(pi/32) q[7],q[2];
-crz(pi/16) q[6],q[2];
-crz(pi/8) q[5],q[2];
-crz(pi/4) q[4],q[2];
-crz(pi/2) q[3],q[2];
+cp(pi/128) q[9],q[2];
+cp(pi/64) q[8],q[2];
+cp(pi/32) q[7],q[2];
+cp(pi/16) q[6],q[2];
+cp(pi/8) q[5],q[2];
+cp(pi/4) q[4],q[2];
+cp(pi/2) q[3],q[2];
 h q[2];
-crz(pi/256) q[9],q[1];
-crz(pi/128) q[8],q[1];
-crz(pi/64) q[7],q[1];
-crz(pi/32) q[6],q[1];
-crz(pi/16) q[5],q[1];
-crz(pi/8) q[4],q[1];
-crz(pi/4) q[3],q[1];
-crz(pi/2) q[2],q[1];
+cp(pi/256) q[9],q[1];
+cp(pi/128) q[8],q[1];
+cp(pi/64) q[7],q[1];
+cp(pi/32) q[6],q[1];
+cp(pi/16) q[5],q[1];
+cp(pi/8) q[4],q[1];
+cp(pi/4) q[3],q[1];
+cp(pi/2) q[2],q[1];
 h q[1];
-crz(pi/512) q[9],q[0];
-crz(pi/256) q[8],q[0];
-crz(pi/128) q[7],q[0];
-crz(pi/64) q[6],q[0];
-crz(pi/32) q[5],q[0];
-crz(pi/16) q[4],q[0];
-crz(pi/8) q[3],q[0];
-crz(pi/4) q[2],q[0];
-crz(pi/2) q[1],q[0];
+cp(pi/512) q[9],q[0];
+cp(pi/256) q[8],q[0];
+cp(pi/128) q[7],q[0];
+cp(pi/64) q[6],q[0];
+cp(pi/32) q[5],q[0];
+cp(pi/16) q[4],q[0];
+cp(pi/8) q[3],q[0];
+cp(pi/4) q[2],q[0];
+cp(pi/2) q[1],q[0];
 h q[0];
+swap q[0],q[9];
+swap q[1],q[8];
+swap q[2],q[7];
+swap q[3],q[6];
+swap q[4],q[5];
 """
 
 import os
@@ -111,21 +120,70 @@ def save_string_to_file(filename, content, mode='w', encoding='utf-8', line_endi
 
 
 class TestLSInstructionsFromGatesGenerator:
-    def test_text_from_gates_circuit(self):
+    def test_text_from_gates_circuit(self,qasmstr=None):
+        if not qasmstr:
+            qasmstr = TEST2
+
+        clifford_plus_t = GatesCircuit.from_qasm(qasmstr).to_clifford_plus_t()
+        #print(clifford_plus_t)
         instructions: str = LSInstructionsFromGatesGenerator.text_from_gates_circuit(
-                GatesCircuit.from_qasm(TEST).to_clifford_plus_t()
+                clifford_plus_t
             )
+
         print('instructions=\n',instructions)
+        return instructions
         # instructions: str = repr(
         #     LSInstructionsFromGatesGenerator.text_from_gates_circuit(
         #         GatesCircuit.from_qasm(QFT_CIRCUIT).to_clifford_plus_t()
         #     )
         # )
-        save_string_to_file('d:/ls.txt', instructions, mode='w', encoding='utf-8')
+        #save_string_to_file('d:/ls.txt', instructions, mode='w', encoding='utf-8')
         # snapshot.assert_match(instructions, "circuit.txt")
 
     def test_text_from_gates_circuit_fail_non_clifford_plus_t(self):
         circuit = GatesCircuit.from_qasm(QFT_CIRCUIT)
-        with pytest.raises(Exception):
-            LSInstructionsFromGatesGenerator.text_from_gates_circuit(circuit)
+        # with pytest.raises(Exception):
+        output = LSInstructionsFromGatesGenerator.text_from_gates_circuit(circuit)
+        print(output)
+
+
+
+    def test_convert_all_file(self):
+        input_directory = 'd:/sync/mqtbench/ori'
+        output_directory = 'd:/sync/mqtbench/out'
+        for filename in os.listdir(input_directory):
+            input_file_path = os.path.join(input_directory, filename)
+            content = ''
+            if os.path.isfile(input_file_path):
+                with open(input_file_path, 'r') as infile:
+                    lines = infile.readlines()  # Read all lines from the file
+                    print(lines)
+                    for line in lines:
+                        if line.startswith('creg') or line.startswith(r'//') or line.startswith('barrier') or line.startswith('measure'):
+                            continue
+                        line = line.strip().replace('cp', 'crz')+'\n'
+                        content += line
+                    # content = infile.read()
+
+                # Process the lines
+                    #replace all 'cp' to 'crz'
+                # content = content.replace('cp', 'crz')
+                print(content)
+                content = self.test_text_from_gates_circuit(qasmstr=content)
+                # Write the processed lines to a new file in the output directory
+                output_file_path = os.path.join(output_directory, f"processed_{filename}")
+                with open(output_file_path, 'w') as outfile:
+                    outfile.writelines(content)
+
+
+
+    # def test_convert_to_basic_gates(self,qasm:str=''):
+    #     qasm =TEST
+    #     basic_gates = ['s','x','cx','rz','crz','t','h']
+    #     new_circuit = QuantumCircuit.from_qasm_str(qasm)
+    #     c = new_circuit.decompose(gates_to_decompose = basic_gates)
+    #     print(c)
+    #     #convert circuit to qasm file
+    #     qasm = c.qasm()
+    #     print(qasm)
 
