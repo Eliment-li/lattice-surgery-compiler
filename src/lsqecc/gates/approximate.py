15714acf7dff4da0a65c0843fe5cf7fe67363182
Eliment-li <felix.litian@foxmail.com>
2025-06-30 20:16:06 +0800

    feat support rccx ccx, and Tdag_to_T

--- src/lsqecc/gates/approximate.py
+++ src/lsqecc/gates/approximate.py
@@ -2,6 +2,8 @@ import math
 from fractions import Fraction
 from typing import Sequence
 
+from setuptools.unicode_utils import decompose
+
 from lsqecc.gates import gates
 from lsqecc.gates.compress_rotation_approximations import partition_gate_sequence
 from lsqecc.gates.pi_over_2_to_the_n_rz_gate_approximations import (
@@ -11,6 +13,7 @@ from lsqecc.pauli_rotations.rotation import PauliOperator
 from lsqecc.utils import is_power_of_two
 import pennylane as qml
 import numpy as np
+from itertools import chain
 
 def handle_sk_decompose_ops(ops:list,compress_rotations:bool,target_qubit:int) -> Sequence["gates.Gate"]:
     Tdg = 'Adjoint(T(0))'
@@ -36,24 +39,26 @@ def handle_sk_decompose_ops(ops:list,compress_rotations:bool,target_qubit:int) -
             )
         elif str(o) == Tdg:
             # T dagger = Z-S-T = hxh-s-t
-            approx_gates.append(gates.H(target_qubit))
-            approx_gates.append(gates.X(target_qubit))
-            approx_gates.append(gates.H(target_qubit))
-            # s
-            approx_gates.append(
-                gates.PauliRotations(
-                    target_qubit=target_qubit, phase=Fraction(1, 2), axis=PauliOperator.Z
-                )
-                if compress_rotations
-                else gates.S(target_qubit)
-            )  # t
-            approx_gates.append(
-                gates.PauliRotations(
-                    target_qubit=target_qubit, phase=Fraction(1, 4), axis=PauliOperator.Z
-                )
-                if compress_rotations
-                else gates.T(target_qubit)
-            )
+            # approx_gates.append(gates.H(target_qubit))
+            # approx_gates.append(gates.X(target_qubit))
+            # approx_gates.append(gates.H(target_qubit))
+            # # s
+            # approx_gates.append(
+            #     gates.PauliRotations(
+            #         target_qubit=target_qubit, phase=Fraction(1, 2), axis=PauliOperator.Z
+            #     )
+            #     if compress_rotations
+            #     else gates.S(target_qubit)
+            # )  # t
+            # approx_gates.append(
+            #     gates.PauliRotations(
+            #         target_qubit=target_qubit, phase=Fraction(1, 4), axis=PauliOperator.Z
+            #     )
+            #     if compress_rotations
+            #     else gates.T(target_qubit)
+            # )
+           approx_gates += Tdag_to_T(target_qubit,compress_rotations)
+
         elif str(o) == 'GlobalPhase(array(0.), wires=[])' or isinstance(o, qml.ops.GlobalPhase):
             continue
         else:
@@ -64,7 +69,6 @@ def approximate_p_gate(p_gate:"gates.P",compress_rotations)-> Sequence["gates.Ga
     #PhaseShift is  the P gate in qasm and qiskit, it  is equivalent to RZ up to a phase factor.
     op = qml.PhaseShift(p_gate.theta,wires=0)
     ops = qml.ops.sk_decomposition(op, epsilon=1e-3)
-
     approx_gates = handle_sk_decompose_ops(ops, compress_rotations, p_gate.target_qubit)
     return approx_gates
 
@@ -74,7 +78,9 @@ def approximate_u_gate(u_gate:"gates.U",compress_rotations)-> Sequence["gates.Ga
     approx_gates = handle_sk_decompose_ops(ops, compress_rotations, u_gate.target_qubit)
     return approx_gates
 
-def approximate_rccx_gate(u_gate:"gates.RCCX",compress_rotations=False)-> Sequence["gates.Gate"]:
+
+
+def decompose_rccx_gate(u_gate:"gates.RCCX",compress_rotations=False)-> Sequence["gates.Gate"]:
     '''
     The Margolus gate can be decomposed using 3 CNOT gates
     Ry(pi/4) q[2]
@@ -192,7 +198,81 @@ def from_gate_string(target_qubit: int, gate_string: str):
             target_qubit, phase=count_s_and_t_to_phase(gate_string), axis=PauliOperator.Z
         )
 
+
+
+def decompose_ccx_gate(u_gate:"gates.CCX",compress_rotations=False)-> Sequence["gates.Gate"]:
+    '''
+    H q[2]
+    CNOT q[1],q[2]
+    Tdag q[2]
+    CNOT q[0],q[2]
+    T q[2]
+    CNOT q[1],q[2]
+    Tdag q[2]
+    CNOT q[0],q[2]
+    T q[1:2]
+    CNOT q[0],q[1]
+    H q[2]
+    T q[0]
+    Tdag q[1]
+    CNOT q[0],q[1]
+    '''
+    target_qubit = u_gate.target_qubit
+    approx_gates = list(chain(
+        [
+        gates.H(u_gate.target_qubit),
+        gates.CNOT(control_qubit=u_gate.control_qubit_1, target_qubit=target_qubit)
+        ],
+        Tdag_to_T(target_qubit, compress_rotations),
+        [gates.CNOT(control_qubit=u_gate.control_qubit_0, target_qubit=target_qubit)],
+        #T
+        [get_T(target_qubit, compress_rotations)],
+        [gates.CNOT(control_qubit=u_gate.control_qubit_1, target_qubit=target_qubit)],
+        Tdag_to_T(target_qubit, compress_rotations),
+        [gates.CNOT(control_qubit=u_gate.control_qubit_0, target_qubit=target_qubit)],
+        #T q[1:2]
+        [get_T(u_gate.control_qubit_1, compress_rotations)],
+        [get_T(u_gate.target_qubit, compress_rotations)],
+        [gates.CNOT(control_qubit=u_gate.control_qubit_0, target_qubit=target_qubit)],
+        [gates.H(u_gate.target_qubit)],
+        [get_T(u_gate.control_qubit_0, compress_rotations)],
+        Tdag_to_T(u_gate.control_qubit_1, compress_rotations),
+        [gates.CNOT(control_qubit=u_gate.control_qubit_0, target_qubit=target_qubit)],
+    ))
+
+    return approx_gates
+
+def Tdag_to_T(target_qubit:int,compress_rotations:bool) -> list["gates.Gate"]:
+    """
+    Returns the T gate that is equivalent to T dagger on the target qubit.
+    """
+
+    return [
+        gates.H(target_qubit),
+        gates.X(target_qubit),
+        gates.H(target_qubit),
+        gates.PauliRotations(
+            target_qubit=target_qubit, phase=Fraction(1, 2), axis=PauliOperator.Z
+        )
+        if compress_rotations
+        else gates.S(target_qubit),
+        gates.PauliRotations(
+            target_qubit=target_qubit, phase=Fraction(1, 4), axis=PauliOperator.Z
+        )
+        if compress_rotations
+        else gates.T(target_qubit)
+    ]
+
+
+def get_T(target_qubit:int,compress_rotations:bool)->"gates.T":
+    """
+    Returns the T gate that is equivalent to T dagger on the target qubit.
+    """
+    return gates.PauliRotations(
+            target_qubit=target_qubit, phase=Fraction(1, 4), axis=PauliOperator.Z
+        ) if compress_rotations else gates.T(target_qubit)
+
 if __name__ == '__main__':
     gate = gates.RCCX(control_qubit_0=0, control_qubit_1=1, target_qubit=2)
-    ret = approximate_rccx_gate(gate,compress_rotations=False)
+    ret = decompose_rccx_gate(gate,compress_rotations=False)
     print(ret)
